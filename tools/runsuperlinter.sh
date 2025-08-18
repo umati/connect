9f333a8b7b1fb7620a2f4df2dccf6d4b6a22572b
Goetz Goerisch <g.goerisch@vdw.de>
2025-08-18 10:48:01 +0200

    refactor: fix documentation and license information
    * add missing license headers
    
    Signed-off-by: Goetz Goerisch <g.goerisch@vdw.de>

--- tools/runsuperlinter.sh
+++ tools/runsuperlinter.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+# Copyright 2024 (c) Moritz Walker, ISW University of Stuttgart (for umati and VDW e.V.)
+# Copyright 2025 (c) Goetz Goerisch, VDW e.V.
 
 # Get the current directory
 REPO_DIR=$(pwd)
 echo "$REPO_DIR"
 
+# pull the latest super-linter image
+docker pull ghcr.io/super-linter/super-linter:latest
+
 # Run the Docker container with the specified environment variables and volume mount
 docker run \
 	-e IGNORE_GITIGNORED_FILES=true \
 	-e LOG_LEVEL=INFO \
-	-e DEFAULT_BRANCH=main \
+	-e DEFAULT_BRANCH=origin/main \
 	-e VALIDATE_ALL_CODEBASE=true \
 	-e VALIDATE_XML=false \
 	-e RUN_LOCAL=true \
